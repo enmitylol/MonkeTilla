2244ea274d2b696a850277d4ba3d3b3c2d30c721
enmitylol <f8vrone@gmail.com>
2025-09-01 11:57:10 -0400

    upload the right file
    other one was old

--- MonkeTilla.cs
+++ MonkeTilla.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
@@ -6,15 +8,15 @@ using Photon.Pun;
 public class MonkeTilla : BaseMod
 {
     public static bool IsInModdedLobby { get; internal set; }
-    internal static bool IsModdedQueueSelected;
     private static GameObject managerObject;
+    internal static List<string> CustomQueues = new List<string>();
 
     public override void OnLoad()
     {
         if (managerObject == null)
         {
             managerObject = new GameObject("MonkeTillaManager");
-            managerObject.AddComponent<ModdedQueueManager>();
+            managerObject.AddComponent<CustomQueueManager>();
             GameObject.DontDestroyOnLoad(managerObject);
         }
     }
@@ -26,40 +28,76 @@ public class MonkeTilla : BaseMod
             GameObject.Destroy(managerObject);
         }
     }
+
+    public static bool RegisterCustomQueue(string queueName)
+    {
+        if (CustomQueues.Count >= 3 || string.IsNullOrEmpty(queueName) || CustomQueues.Contains(queueName.ToUpper()))
+        {
+            return false;
+        }
+        
+        CustomQueues.Add(queueName.ToUpper());
+        
+        if (managerObject != null)
+        {
+            var manager = managerObject.GetComponent<CustomQueueManager>();
+            if (manager != null)
+            {
+                manager.OnCustomQueueRegistered();
+            }
+        }
+        return true;
+    }
 }
 
-public class ModdedQueueManager : MonoBehaviourPunCallbacks
+public class CustomQueueManager : MonoBehaviourPunCallbacks
 {
-    private GameObject moddedButton;
+    private List<string> allQueues = new List<string> { "DEFAULT", "COMPETITIVE", "MODDED" };
+    private int currentQueueIndex = 0;
+
     private MeshRenderer moddedButtonRenderer;
+    private Text queueDisplayText;
+    private Text defaultText;
+    private Text competitiveText;
+    private Text moddedText;
+    
+    private GorillaQueue gorillaQueue;
+    private PhotonNetworkController networkController;
 
     void Start()
     {
-        CreateModdedQueueButton();
+        gorillaQueue = GorillaQueue.instance;
+        networkController = PhotonNetworkController.instance;
+        
+        if (gorillaQueue == null) return;
+        
+        defaultText = FindText("DefaultQueueText");
+        competitiveText = FindText("competitivequeuetext");
+
+        CreateButtonsAndText();
+        UpdateVisuals();
+    }
+    
+    public void OnCustomQueueRegistered()
+    {
+        allQueues = new List<string> { "DEFAULT", "COMPETITIVE", "MODDED" };
+        allQueues.AddRange(MonkeTilla.CustomQueues);
+        UpdateVisuals();
     }
 
     void Update()
     {
-        if (GorillaQueue.instance != null && (GorillaQueue.instance.currentQueue == "COMPETITIVE" || GorillaQueue.instance.currentQueue == "DEFAULT"))
+        if (networkController != null)
         {
-            if (MonkeTilla.IsModdedQueueSelected)
-            {
-                MonkeTilla.IsModdedQueueSelected = false;
-                UpdateVisuals();
-            }
-        }
-
-        if (MonkeTilla.IsModdedQueueSelected && PhotonNetworkController.instance != null)
-        {
-            PhotonNetworkController.instance.currentGameType = "MODDED";
+            networkController.currentGameType = allQueues[currentQueueIndex];
         }
     }
 
     public override void OnJoinedRoom()
     {
-        if (PhotonNetwork.InRoom && PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue("gameMode", out object gameMode) && gameMode is string mode && mode == "MODDED")
+        if (PhotonNetwork.InRoom && PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue("gameMode", out object gameMode) && gameMode is string mode)
         {
-            MonkeTilla.IsInModdedLobby = true;
+            MonkeTilla.IsInModdedLobby = (mode != "DEFAULT" && mode != "COMPETITIVE");
         }
         else
         {
@@ -72,99 +110,153 @@ public class ModdedQueueManager : MonoBehaviourPunCallbacks
         MonkeTilla.IsInModdedLobby = false;
     }
 
-    public void SelectModdedQueue()
+    public void CycleQueue(int direction)
     {
-        MonkeTilla.IsModdedQueueSelected = true;
-        GorillaQueue.instance.currentQueue = "MODDED_INTERNAL";
-        PlayerPrefs.SetString("gorillaQueue", "MODDED");
+        if (allQueues.Count <= 3) return;
+
+        currentQueueIndex += direction;
+        if (currentQueueIndex >= allQueues.Count)
+        {
+            currentQueueIndex = 0;
+        }
+        if (currentQueueIndex < 0)
+        {
+            currentQueueIndex = allQueues.Count - 1;
+        }
+        
+        SelectQueueByIndex(currentQueueIndex);
+    }
+    
+    public void SelectQueueByIndex(int index)
+    {
+        if (index < 0 || index >= allQueues.Count) return;
+
+        currentQueueIndex = index;
+        gorillaQueue.currentQueue = allQueues[currentQueueIndex];
+        PlayerPrefs.SetString("gorillaQueue", allQueues[currentQueueIndex]);
         UpdateVisuals();
     }
 
     private void UpdateVisuals()
     {
-        if (GorillaQueue.instance == null || moddedButtonRenderer == null) return;
-
-        bool isModded = MonkeTilla.IsModdedQueueSelected;
-
-        moddedButtonRenderer.material = isModded ? GorillaQueue.instance.defaultMaterial : GorillaQueue.instance.transparentDefaultMaterial;
+        if (gorillaQueue == null) return;
         
-        if (GorillaQueue.instance.competitiveRenderer != null)
+        string selectedQueue = allQueues[currentQueueIndex];
+        bool hasCustomQueues = allQueues.Count > 3;
+
+        if (defaultText != null) defaultText.gameObject.SetActive(!hasCustomQueues);
+        if (competitiveText != null) competitiveText.gameObject.SetActive(!hasCustomQueues);
+        if (moddedText != null) moddedText.gameObject.SetActive(!hasCustomQueues);
+        if (queueDisplayText != null) queueDisplayText.gameObject.SetActive(hasCustomQueues);
+
+        gorillaQueue.defaultRenderer.material = (selectedQueue == "DEFAULT") ? gorillaQueue.defaultMaterial : gorillaQueue.transparentDefaultMaterial;
+        gorillaQueue.competitiveRenderer.material = (selectedQueue == "COMPETITIVE") ? gorillaQueue.competitiveMaterial : gorillaQueue.transparentCompetitiveMaterial;
+        
+        if(moddedButtonRenderer != null)
         {
-            GorillaQueue.instance.competitiveRenderer.material = GorillaQueue.instance.transparentCompetitiveMaterial;
+            moddedButtonRenderer.material = (selectedQueue == "MODDED") ? gorillaQueue.defaultMaterial : gorillaQueue.transparentDefaultMaterial;
         }
-        if (GorillaQueue.instance.defaultRenderer != null)
+
+        if (hasCustomQueues && queueDisplayText != null)
         {
-            GorillaQueue.instance.defaultRenderer.material = GorillaQueue.instance.transparentDefaultMaterial;
+            queueDisplayText.text = selectedQueue;
+            queueDisplayText.fontStyle = (currentQueueIndex > 2) ? FontStyle.Bold : FontStyle.Normal;
+        }
+    }
+    
+    private void CreateButtonsAndText()
+    {
+        GameObject moddedButton = CreateButton("ModdedQueueButton", new Vector3(0.22f, 0.911f, -0.103f));
+        moddedButton.AddComponent<QueueSelectTrigger>().Initialize(this, 2);
+        moddedButtonRenderer = moddedButton.GetComponent<MeshRenderer>();
+        
+        GameObject leftButton = CreateButton("CycleQueueLeftButton", new Vector3(-0.014f, 0.911f, -1.229f));
+        leftButton.AddComponent<QueueCycleTrigger>().Initialize(this, -1);
+        
+        GameObject rightButton = CreateButton("CycleQueueRightButton", new Vector3(-0.014f, 0.911f, 1.2f));
+        rightButton.AddComponent<QueueCycleTrigger>().Initialize(this, 1);
+        
+        if (competitiveText != null)
+        {
+           moddedText = CreateText("ModdedQueueText", "modded", new Vector3(82f, -539f, 1f), competitiveText);
+        }
+
+        if (defaultText != null)
+        {
+            queueDisplayText = CreateText("CustomQueueText", "DEFAULT", defaultText.rectTransform.localPosition, defaultText);
         }
     }
 
-    private void CreateModdedQueueButton()
+    private GameObject CreateButton(string name, Vector3 position)
     {
-        if (GorillaQueue.instance == null || GorillaQueue.instance.competitiveRenderer == null) return;
+        GameObject originalButton = gorillaQueue.competitiveRenderer.gameObject;
+        GameObject newButton = Instantiate(originalButton, originalButton.transform.parent);
+        newButton.name = name;
+        newButton.transform.localPosition = position;
+        Destroy(newButton.GetComponent<GorillaQueueChoice>());
+        return newButton;
+    }
 
-        GameObject originalButton = GorillaQueue.instance.competitiveRenderer.gameObject;
-        moddedButton = Instantiate(originalButton, originalButton.transform.parent);
-        moddedButton.name = "ModdedQueueButton";
-        moddedButton.transform.localPosition = new Vector3(0.22f, 0.911f, -0.103f);
-
-        GorillaQueueChoice oldTrigger = moddedButton.GetComponent<GorillaQueueChoice>();
-        if (oldTrigger != null)
+    private Text FindText(string name)
+    {
+        foreach (Text t in Resources.FindObjectsOfTypeAll<Text>())
         {
-            Destroy(oldTrigger);
+            if (t.gameObject.name == name) return t;
         }
-
-        moddedButtonRenderer = moddedButton.GetComponent<MeshRenderer>();
-        moddedButtonRenderer.material = GorillaQueue.instance.transparentDefaultMaterial;
-
-        var trigger = moddedButton.AddComponent<ModdedQueueButtonTrigger>();
-        trigger.manager = this;
-
-        GameObject originalTextObject = null;
-        Text[] allTexts = Resources.FindObjectsOfTypeAll<Text>();
-        foreach (Text t in allTexts)
-        {
-            if (t.gameObject.name == "competitivequeuetext")
-            {
-                originalTextObject = t.gameObject;
-                break;
-            }
-        }
+        return null;
+    }
+    
+    private Text CreateText(string name, string content, Vector3 position, Text prefab)
+    {
+        if(prefab == null) return null;
         
-        if (originalTextObject != null)
-        {
-            GameObject moddedButtonText = Instantiate(originalTextObject, originalTextObject.transform.parent);
-            moddedButtonText.SetActive(true);
-            moddedButtonText.name = "ModdedQueueText";
+        GameObject textObject = Instantiate(prefab.gameObject, prefab.transform.parent);
+        textObject.SetActive(true);
+        textObject.name = name;
+        
+        var rectTransform = textObject.GetComponent<RectTransform>();
+        rectTransform.localPosition = position;
 
-            var rectTransform = moddedButtonText.GetComponent<RectTransform>();
-            if(rectTransform != null)
-            {
-                 rectTransform.localPosition = new Vector3(82f, -539f, 1f);
-            }
-            else
-            {
-                moddedButtonText.transform.localPosition = new Vector3(82f, -539f, 1f);
-            }
-           
-            Text textComponent = moddedButtonText.GetComponent<Text>();
-            if (textComponent != null)
-            {
-                textComponent.text = "Modded";
-            }
-        }
+        Text textComponent = textObject.GetComponent<Text>();
+        textComponent.text = content;
+
+        return textComponent;
     }
 }
 
-public class ModdedQueueButtonTrigger : GorillaTriggerBox
+public class QueueCycleTrigger : GorillaTriggerBox
 {
-    public ModdedQueueManager manager;
+    private CustomQueueManager manager;
+    private int direction;
+
+    public void Initialize(CustomQueueManager manager, int direction)
+    {
+        this.manager = manager;
+        this.direction = direction;
+    }
 
     public override void OnBoxTriggered()
     {
         base.OnBoxTriggered();
-        if (manager != null)
-        {
-            manager.SelectModdedQueue();
-        }
+        manager?.CycleQueue(direction);
     }
 }
+
+public class QueueSelectTrigger : GorillaTriggerBox
+{
+    private CustomQueueManager manager;
+    private int queueIndexToSelect;
+
+    public void Initialize(CustomQueueManager manager, int index)
+    {
+        this.manager = manager;
+        this.queueIndexToSelect = index;
+    }
+    
+    public override void OnBoxTriggered()
+    {
+        base.OnBoxTriggered();
+        manager?.SelectQueueByIndex(queueIndexToSelect);
+    }
+}
+
